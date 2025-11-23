68d90e301fee063f042d2232a915c88b1e4e973f
anacarolinaacla-dot <anacarolina.acla@gmail.com>
2025-11-22 23:20:15 -0300

    desenvolvimento_desafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,47 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+    
+    char estado;
+    char carta[4];
+    char cidade[50];
+    int populacao;
+    float area;
+    float PIB;
+    int pontos_turisticos;
+  
   // Área para entrada de dados
 
+    printf("Estado (A-H): ");
+    scanf("   %c", &estado);
+
+    printf("Código: ");
+    scanf("%s", carta);   
+    
+    printf("Nome da Cidade: ");
+    scanf("%s", cidade); //não usar espaço no nome da cidade
+
+    printf("População: ");
+    scanf("%d", &populacao);
+
+    printf("Área: ");
+    scanf("%f", &area);
+
+    printf("PIB: ");
+    scanf("%f", &PIB);
+
+    printf("Número de Pontos Turísticos: ");
+    scanf("%d", &pontos_turisticos);
+
   // Área para exibição dos dados da cidade
+  
+    printf("Estado: %c\n", estado);
+    printf("Código: %s\n", carta);
+    printf("Nome da Cidade: %s\n", cidade);
+    printf("População: %d\n", populacao);
+    printf("Área: %f\n", area);
+    printf("PIB: %f\n", PIB);
+    printf("Número de Pontos Turísticos: %d", pontos_turisticos);
 
 return 0;
 } 
